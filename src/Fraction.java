0fea5ab305a318d4ff920c6f8580672f8faf0055
kristine bermoy <kbermoy02@gmail.com>
2023-06-28 20:03:06 -0700

    First commit

---
+++ src/Fraction.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.io.File;
+public class Fraction {
+    // data fields
+    ArrayList<Integer> numerators = new ArrayList<Integer>();
+    ArrayList<Integer> denominators = new ArrayList<Integer>();
+
+
+    /**
+     * Fraction()
+     * ----------
+     * Constructor #1:
+     * PRE: denominator != 0
+     */
+    public Fraction() {
+        
+    }
+
+    // first: read file
+
+    // while reading file, reduce the fraction first, then store it in the arrayList
+
+    // call the FractionCounter to count the fraction card with each element int the arrayList
+}
